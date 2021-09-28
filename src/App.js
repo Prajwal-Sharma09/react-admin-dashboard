57c3c2a1c3c002adfc77c3d81c4e8dad3f336787
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2021-09-28 23:18:28 +0545

    added sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   );
 };
