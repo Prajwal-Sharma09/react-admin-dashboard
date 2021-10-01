e2c09d92ab3dc992dc1cfaf67f46bb24603e57f7
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2021-10-01 15:02:07 +0545

    implemented userlist page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import UserList from "./pages/userList/UserList";
 import Home from "./pages/home/Home";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 };
 
