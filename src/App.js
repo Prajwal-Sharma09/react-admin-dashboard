7fb6ec5135fcd7867ebc03dc8bc3ccbfbf74e707
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2021-10-02 18:19:40 +0545

    added new user page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/home/Home";
 import User from "./pages/user/User";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NewUser from "./pages/newUser/NewUser";
 
 const App = () => {
   return (
@@ -22,6 +23,9 @@ const App = () => {
           </Route>
           <Route path="/user/:userId">
             <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
           </Route>
         </Switch>
       </div>
